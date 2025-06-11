97d349d4ace1cc019674f85bef79ae8caa3901c6
sally An <sally03915@gmail.com>
2025-06-12 05:58:26 +0900

    ver-2  front  /  aws  - package.json, config, saga-index.js

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -5,10 +5,10 @@ import postSaga from './post';
 import userSaga from './user';
 //import { backUrl } from '../config/config';
 
-// axios.defaults.baseURL = backUrl;
-// axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:3065';  // 기본요청 url 설정
-axios.defaults.withCredentials = true;  // 쿠키 http요청에 포함시킬건지 설정
+axios.defaults.baseURL = backUrl;
+axios.defaults.withCredentials = true;
+//axios.defaults.baseURL = 'http://localhost:3065';  // 기본요청 url 설정
+//axios.defaults.withCredentials = true;  // 쿠키 http요청에 포함시킬건지 설정
 
 export default function* rootSaga() {
   yield all([
