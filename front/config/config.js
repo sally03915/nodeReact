8c8580ff888f2793d833d3f0ef89391559ccfde8
sally An <sally03915@gmail.com>
2025-06-12 22:28:41 +0900

    ver2 up

--- front/config/config.js
+++ front/config/config.js
@@ -1,3 +1,3 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? 'http://54.180.90.29' : 'http://localhost:3065';
+export const backUrl = process.env.NODE_ENV === 'production' ? 'http://3.34.136.77' : 'http://localhost:3065';
 
 //export const backUrl = process.env.NODE_ENV === 'production' ? 'https://d2big.com' : 'http://localhost:3065';
--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,4 +1,3 @@
-// [store] - configureStore.js
 import React from 'react';
 import { createWrapper } from 'next-redux-wrapper';
 import { createStore, applyMiddleware, compose } from 'redux';
@@ -9,19 +8,25 @@ import rootSaga from '../sagas';
 
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
-  const middlewares = [sagaMiddleware];  //##
+  const middlewares = [sagaMiddleware];
 
-  // middleware 적용 방식 수정
-  const enhancer = process.env.NODE_ENV === 'production' ?
-    compose([...middlewares]) :
-    composeWithDevTools(applyMiddleware(...middlewares))
+  // Middleware 적용 방식 수정
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(applyMiddleware(...middlewares));
 
   const store = createStore(reducer, enhancer);
 
-  store.sagaTask = sagaMiddleware.run(rootSaga);  //##
+  // Redux-Saga 실행 환경 확인 (SSR 방지)
+  if (typeof window !== 'undefined') {
+    store.sagaTask = sagaMiddleware.run(rootSaga);
+  }
+
   return store;
 };
+
 const wrapper = createWrapper(configureStore, {
   debug: process.env.NODE_ENV === 'development',
 });
+
 export default wrapper;
