69698ad29163bc07f176675019c4189b84847208
sally An <sally03915@gmail.com>
2025-06-18 16:32:03 +0900

    domain -  80port

--- front/config/config.js
+++ front/config/config.js
@@ -1,3 +1,4 @@
 //export const backUrl = 'http://13.125.210.230';
 //export const backUrl = process.env.NODE_ENV === 'production' ? 'http://13.125.210.230' : 'http://localhost:3065';//
-export const backUrl = process.env.NODE_ENV === 'production' ? 'https://d2big.com' : 'http://localhost:3065';
+//export const backUrl = process.env.NODE_ENV === 'production' ? 'http://d2bigs.com' : 'http://localhost:3065';
+export const backUrl = process.env.NODE_ENV === 'production' ? 'http://d2big.com' : 'http://localhost:3065';
